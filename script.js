e740dfd675ebdb6d82ac4ed558a102128bb45b6c
AleksiDu <aleksi.duluzauri@gmail.com>
2022-09-29 16:56:13 +0400

    add local save functuality

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const errorElementLast = document.getElementById('errorLast');
 const errorElementAddress = document.getElementById('errorAddress');
 const rootElement = document.documentElement;
 
+
 /**
  * Validation
  */
@@ -67,7 +68,6 @@ let dataTable = {
     stringAddress: "",
     stringBirth: "",
     stringGender: "",
-    stringNotes: ""
 };
 
 // increment by one every submit clicks
@@ -83,7 +83,6 @@ function submitTable() {
     dataTable.stringAddress = address.value;
     dataTable.stringBirth = birthday.value;
     dataTable.stringGender = gender.value;
-    dataTable.stringNotes = notes.value;
 
     // insert row
 
@@ -96,23 +95,37 @@ function submitTable() {
     let stringBirth = row.insertCell(4);
     let stringGender = row.insertCell(5);
 
-    //  let stringNotes = row.insertCell(5);
     number.innerHTML = i;
     stringFirstName.innerHTML = dataTable.stringFirstName;
     stringLastName.innerHTML = dataTable.stringLastName;
     stringAddress.innerHTML = dataTable.stringAddress;
     stringBirth.innerHTML = dataTable.stringBirth;
     stringGender.innerHTML = dataTable.stringGender;
-    // stringNotes.innerHTML = dataTable.stringNotes;
 
     i++;
-
-    localStorage.setItem("firstName", dataTable.stringFirstName);
-    localStorage.setItem("lastName", dataTable.stringLastName);
-    localStorage.setItem("address", dataTable.stringAddress);
-    localStorage.setItem("birthday", dataTable.stringBirth);
-    localStorage.setItem("gender", dataTable.stringGender);
-    localStorage.setItem("notes", dataTable.stringNotes);
-    localStorage.setItem("display", "block");
-
 };
+
+/**
+ * Local Save
+ */
+
+firstName.value = getSavedValue("firstName");
+lastName.value = getSavedValue("lastName");
+address.value = getSavedValue("address");
+birthday.value = getSavedValue("birthday");
+gender.value = getSavedValue("gender");
+notes.value = getSavedValue("notes");
+
+//Save the value function - save it to localStorage as (ID, VALUE)
+function saveValue(e) {
+    let id = e.id;  // get the sender's id to save it . 
+    let val = e.value; // get the value. 
+    localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
+}
+//get the saved value function - return the value of "v" from localStorage. 
+function getSavedValue(v) {
+    if (!localStorage.getItem(v)) {
+        return "";// Defualt value. 
+    }
+    return localStorage.getItem(v);
+}
